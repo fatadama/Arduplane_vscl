5dccf2a94d626b96587a2dd58f9bd79ac1a0e31d
tdw <cdr.awesomebeard@gmail.com>
2014-05-02 20:54:28 -0500

    Null

--- ArduPlane_vscl.ino
+++ ArduPlane_vscl.ino
@@ -676,7 +676,7 @@ AP_Mount camera_mount2(&current_loc, g_gps, &ahrs, 1);
 //pinMode(camtrig, OUTPUT);			// these are free pins PE3(5), PH3(15), PH6(18), PB4(23), PB5(24), PL1(36), PL3(38), PA6(72), PA7(71), PK0(89), PK1(88), PK2(87), PK3(86), PK4(83), PK5(84), PK6(83), PK7(82)
 #endif
 
-//VSCL globals: VSCL_PHI: commanded bank angle from the ground
+//VSCL globals: VSCL_PHI: commanded bank angle from the ground, IN DEGREES
 int16_t VSCL_PHI = 0;
 int16_t VSCL_SPD = 1100;//11 m/s by default
 int16_t VSCL_ALT = 6069;//60.69 m by default
